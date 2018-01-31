1258209268ffb31cb1f1410bc48ba5482f8851d6
jaine <jayne-conceicao@hotmail.com>
2018-01-31 18:08:48 -0300

    Versão 1

---
+++ spellchecker/Dictionary.py
@@ -0,0 +1,29 @@
+class Dictionary(object):
+    
+    #Construct an empty dictionary
+    def __init__(self):
+        self.dict = []
+    
+    #Load this dictionary from filename
+    def load(self, filename):
+        fileDic = open(filename, 'r')
+        linhas = fileDic.readlines()
+        for wd in linhas:
+            self.dict.append(wd)  
+        
+    #Save this dictionary to filename    
+    def save(self, filename):
+        self.dict.sort()
+        fileDic = open(filename, 'w')
+        for wd in self.dict:
+            fileDic.write(wd)
+            
+    #Check if a word wd is in this dictionary        
+    def contains(self, wd):
+        for line in self.dict:
+            if wd in line:
+                return True
+
+    #Add a word wd to this dictionary            
+    def add(self, wd):
+        self.dict.append(wd+"\n")
---
+++ spellchecker/Main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from spellchecker.Dictionary import Dictionary
+
+#If a word is unknown, ask the user what to do with it
+#The user can accept the word, ignore it or replace it
+def consultUser(currentWord, mainDict, ignored):
+    print("A palavra "+currentWord+" é desconhecida. O que você deseja fazer?")
+    print("1 - Adicionar a palavra ao dicionário")
+    print("2 - Ignorar a palavra")
+    print("3 - Substituir a palavra por outra")
+    opcao = int(input("Opção: "))
+    
+    if opcao == 1:
+        mainDict.add(currentWord)
+        return currentWord
+    if opcao == 2:
+        ignored.add(currentWord)
+        return currentWord
+    if opcao == 3:
+        newWord = input("Insira a nova palavra: ")
+        return newWord
+       
+       
+#Copy all words and punctuation from the input document to the output document
+#but ask the user what to do with any words that are unknown        
+def processDocument(mainDict, ignored):
+    inDoc = open('in.txt', 'r')
+    outDoc = open('out.txt', 'w')
+    words = inDoc.readlines()
+    for line in words:
+        for currentWord in line.split():
+            if not mainDict.contains(currentWord) and not ignored.contains(currentWord):
+                currentWord = consultUser(currentWord, mainDict, ignored)
+            outDoc.write(currentWord+" ")
+    inDoc.close()
+    outDoc.close()        
+    print("Verificação ortográfica finalizada com sucesso!")    
+        
+
+#Main function
+if __name__ == '__main__':
+    mainDict = Dictionary()
+    ignored = Dictionary()
+    try:
+        mainDict.load('main-dict.txt')
+        processDocument(mainDict, ignored)
+        mainDict.save('main-dict.txt')
+    except:
+        print("ferrou")
+    
